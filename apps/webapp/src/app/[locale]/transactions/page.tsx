efd68a2798e6a62ecf3fd68555ba70c609ce5f04
Maïa PERDERIZET <dev.info@capronpodologie.com>
2025-09-19 09:16:13 +0200

    feat: transaction form

--- apps/webapp/src/app/[locale]/transactions/page.tsx
+++ apps/webapp/src/app/[locale]/transactions/page.tsx
@@ -1,26 +1,58 @@
-import {CreateTransitionForm} from "./create/CreateTransactionForm";
-import {prisma} from "@/lib/prisma";
+'use client';
+import { CreateTransitionForm } from './create/CreateTransactionForm';
+import { prisma } from '@/lib/prisma';
+import { Euro } from 'lucide-react';
+import DatePicker from 'react-datepicker';
+import { useEffect, useState } from 'react';
+export default function Page() {
+  const [date, setDate] = useState<Date | null>(null);
+  const [dataTransactions, setDataTransactions] = useState<any[]>([]);
 
+  useEffect(() => {
+    dataTransactions;
+  }, [dataTransactions]);
 
-export default async function Page(){
-    const transactionCount = await prisma.transaction.count();
+  useEffect(() => {
+    const fetchTransactions = async () => {
+      const res = await fetch(
+        `/api/transactions?date=${(date || new Date()).toISOString()}`
+      );
+      const json = await res.json();
+      setDataTransactions(json);
+    };
+    fetchTransactions();
+  }, [date]);
 
-    const stats = [
-        { title: 'Total Transaction', value: transactionCount, description: 'Current rows' },
-    ];
-    return (
-        <>
-            <CreateTransitionForm/>
+  return (
+    <>
+      <CreateTransitionForm />
+      <DatePicker
+        selected={date}
+        onChange={date => setDate(date)}
+        showTimeSelect
+      />
+      <div className="mt-4">
+        {dataTransactions.map(transaction => (
+          <div key={transaction.id}>
+            <h3>{transaction.createdAt.toLocaleDateString()}</h3>
+            <div className="border-b flex justify-between py-2">
+              <span>{transaction.category?.name || 'No Category'}</span>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-                {stats.map((s) => (
-                    <div key={s.title}>
-                        <div className="text-sm">{s.title}</div>
-                        <div className="text-2xl font-bold">{s.value}</div>
-                        <div className="text-xs text-muted-foreground">{s.description}</div>
-                    </div>
-                ))}
+              {transaction.type === 'INCOME' ? (
+                <span className="flex items-center text-green-500">
+                  {transaction.amount.toString()}
+                  <Euro size={16} className="ml-1" />
+                </span>
+              ) : (
+                <span className="flex items-center text-red-500">
+                  {transaction.amount.toString()}
+                  <Euro size={16} className="ml-1" />
+                </span>
+              )}
             </div>
-        </>
-    )
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
