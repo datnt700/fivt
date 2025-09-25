4017499b11b840c3914dc326bd5f49cd72669548
Maïa PERDERIZET <dev.info@capronpodologie.com>
2025-09-25 12:02:36 +0200

    feat: header

--- apps/website/src/app/page.tsx
+++ apps/website/src/app/page.tsx
@@ -1,103 +1,95 @@
-import Image from "next/image";
+'use client';
+import Image from 'next/image';
+import logo from '../../public/logo.png';
+import { Menu } from 'lucide-react';
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const MenuItem = [
+    { name: 'Home', link: '#' },
+    { name: 'About', link: '#' },
+    { name: 'Services', link: '#' },
+    { name: 'Contact', link: '#' },
+    { name: 'Blog', link: '#' },
+  ];
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const toggleMenu = () => {
+    const navLinks = document.querySelector('.nav-links');
+    if (navLinks) {
+      if (navLinks.classList.contains('top-[-100%]')) {
+        navLinks.classList.remove('top-[-100%]');
+        navLinks.classList.add('top-[49px]');
+      } else {
+        navLinks.classList.remove('top-[49px]');
+        navLinks.classList.add('top-[-100%]');
+      }
+    }
+  };
+
+  return (
+    <div className="font-[Poppins] bg-gradient-to-t from-[#fbc2eb] to-[#a6c1ee] min-h-screen">
+      <header className="bg-white">
+        <nav className="flex justify-between items-center w-[90%] mx-auto">
+          <div>
+            <Image src={logo} alt="logo" className="w-22 cursor-pointer" />
+          </div>
+          <div className="nav-links duration-500 md:static absolute bg-white md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto  w-full flex items-center px-5">
+            <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
+              {MenuItem.map(menu => (
+                <li key={menu.name}>
+                  <a
+                    className="
+      inline-block
+      border-b-0
+      [background-image:linear-gradient(transparent,transparent),linear-gradient(currentColor,currentColor)]
+      [background-size:0_3px]
+      [background-position:0_100%]
+      bg-no-repeat
+      transition-[background-size] duration-500 ease-in-out
+      hover:[background-size:100%_3px]
+    "
+                    href={menu.link}
+                  >
+                    {menu.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="flex items-center gap-6">
+            <button
+              className="
+                group relative inline-flex items-center justify-center
+                rounded-full px-5 py-2
+                bg-[#a6c1ee] text-white
+                overflow-hidden
+              "
+            >
+              <span
+                className="
+                  absolute inset-0
+                  bg-[#4f7fe6]          
+                  -translate-y-full    
+                  transition-transform duration-500 ease-out
+                  group-hover:translate-y-0
+                "
+                aria-hidden="true"
+              />
+              <span
+                className="
+                relative z-10
+                transition-colors duration-500
+                group-hover:text-white
+              "
+              >
+                Sign in
+              </span>
+            </button>
+            <div onClick={toggleMenu} className="cursor-pointer md:hidden">
+              <Menu />
+            </div>
+          </div>
+        </nav>
+      </header>
     </div>
   );
 }
