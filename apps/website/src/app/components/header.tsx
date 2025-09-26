808a4fc4dab6eef37fbcda7f23a83e91725c3132
Maïa PERDERIZET <dev.info@capronpodologie.com>
2025-09-26 11:23:53 +0200

    feat: add hero banner & footer

---
+++ apps/website/src/app/components/header.tsx
@@ -0,0 +1,97 @@
+import Image from 'next/image';
+import logo from '../../../public/logo.png';
+import { Menu, X } from 'lucide-react';
+export const Header = () => {
+  const MenuItem = [
+    { name: 'Home', link: '#' },
+    { name: 'About', link: '#' },
+    { name: 'Services', link: '#' },
+    { name: 'Contact', link: '#' },
+    { name: 'Blog', link: '#' },
+  ];
+
+  const toggleMenu = () => {
+    const navLinks = document.querySelector('.nav-links');
+    if (navLinks) {
+      if (navLinks.classList.contains('top-[-100%]')) {
+        navLinks.classList.remove('top-[-100%]');
+        navLinks.classList.add('top-0');
+      } else {
+        navLinks.classList.add('top-0');
+        navLinks.classList.add('top-[-100%]');
+      }
+    }
+  };
+  return (
+    <header>
+      <nav className="flex justify-between items-center  mx-auto md:items-center ">
+        <div>
+          <Image src={logo} alt="logo" className="w-26 cursor-pointer" />
+        </div>
+        <div className="nav-links duration-500 md:static absolute md:min-h-fit bg-white md:bg-transparent h-[100vh] md:h-auto left-0 top-[-100%] md:w-auto  w-full flex flex-col items-center px-5 z-50">
+          <div className="flex align-center justify-between w-full py-5 border-b-[0.5px] md:hidden">
+            <Image src={logo} alt="logo" className="w-22 cursor-pointer" />
+            <div onClick={toggleMenu} className=" cursor-pointer ">
+              <X />
+            </div>
+          </div>
+          <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
+            {MenuItem.map(menu => (
+              <li key={menu.name}>
+                <a
+                  className="
+                      inline-block
+                      border-b-0
+                      [background-image:linear-gradient(transparent,transparent),linear-gradient(currentColor,currentColor)]
+                      [background-size:0_3px]
+                      [background-position:0_100%]
+                      bg-no-repeat
+                      transition-[background-size] duration-500 ease-in-out
+                      hover:[background-size:100%_3px]
+                    "
+                  href={menu.link}
+                >
+                  {menu.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="flex items-center gap-6">
+          <button
+            className="
+                group relative inline-flex items-center justify-center
+                rounded-full px-5 py-2
+                bg-[#a6c1ee] text-white
+                overflow-hidden
+              "
+          >
+            <span
+              className="
+                  absolute inset-0
+                  bg-[#4f7fe6]          
+                  -translate-y-full    
+                  transition-transform duration-500 ease-out
+                  group-hover:translate-y-0
+                "
+              aria-hidden="true"
+            />
+            <span
+              className="
+                relative z-10
+                transition-colors duration-500
+                group-hover:text-white
+                text-black
+              "
+            >
+              Sign in
+            </span>
+          </button>
+          <div onClick={toggleMenu} className="cursor-pointer md:hidden">
+            <Menu />
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
--- apps/website/src/app/page.tsx
+++ apps/website/src/app/page.tsx
@@ -1,95 +1,25 @@
 'use client';
+
+import { Header } from './components/header';
+import herobanner from '../../public/herobanner.jpg';
 import Image from 'next/image';
-import logo from '../../public/logo.png';
-import { Menu } from 'lucide-react';
-
+import { Footer } from './components/footer';
 export default function Home() {
-  const MenuItem = [
-    { name: 'Home', link: '#' },
-    { name: 'About', link: '#' },
-    { name: 'Services', link: '#' },
-    { name: 'Contact', link: '#' },
-    { name: 'Blog', link: '#' },
-  ];
-
-  const toggleMenu = () => {
-    const navLinks = document.querySelector('.nav-links');
-    if (navLinks) {
-      if (navLinks.classList.contains('top-[-100%]')) {
-        navLinks.classList.remove('top-[-100%]');
-        navLinks.classList.add('top-[49px]');
-      } else {
-        navLinks.classList.remove('top-[49px]');
-        navLinks.classList.add('top-[-100%]');
-      }
-    }
-  };
-
   return (
-    <div className="font-[Poppins] bg-gradient-to-t from-[#fbc2eb] to-[#a6c1ee] min-h-screen">
-      <header className="bg-white">
-        <nav className="flex justify-between items-center w-[90%] mx-auto">
-          <div>
-            <Image src={logo} alt="logo" className="w-22 cursor-pointer" />
+    <>
+      <div className="font-[Poppins] bg-gradient-to-t from-[#fbc2eb] to-[#a6c1ee] min-h-screen px-5 md:px-20 rounded-b-xl">
+        <Header />
+        <section className="hero w-full">
+          <div className="flex justify-center items-center ">
+            <Image
+              src={herobanner}
+              alt="herobanner"
+              className="rounded-lg w-full h-auto shadow-lg xl:max-h-[700px] 2xl:max-h-[800px] object-cover"
+            ></Image>
           </div>
-          <div className="nav-links duration-500 md:static absolute bg-white md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto  w-full flex items-center px-5">
-            <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
-              {MenuItem.map(menu => (
-                <li key={menu.name}>
-                  <a
-                    className="
-      inline-block
-      border-b-0
-      [background-image:linear-gradient(transparent,transparent),linear-gradient(currentColor,currentColor)]
-      [background-size:0_3px]
-      [background-position:0_100%]
-      bg-no-repeat
-      transition-[background-size] duration-500 ease-in-out
-      hover:[background-size:100%_3px]
-    "
-                    href={menu.link}
-                  >
-                    {menu.name}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className="flex items-center gap-6">
-            <button
-              className="
-                group relative inline-flex items-center justify-center
-                rounded-full px-5 py-2
-                bg-[#a6c1ee] text-white
-                overflow-hidden
-              "
-            >
-              <span
-                className="
-                  absolute inset-0
-                  bg-[#4f7fe6]          
-                  -translate-y-full    
-                  transition-transform duration-500 ease-out
-                  group-hover:translate-y-0
-                "
-                aria-hidden="true"
-              />
-              <span
-                className="
-                relative z-10
-                transition-colors duration-500
-                group-hover:text-white
-              "
-              >
-                Sign in
-              </span>
-            </button>
-            <div onClick={toggleMenu} className="cursor-pointer md:hidden">
-              <Menu />
-            </div>
-          </div>
-        </nav>
-      </header>
-    </div>
+        </section>
+      </div>
+      <Footer />
+    </>
   );
 }
