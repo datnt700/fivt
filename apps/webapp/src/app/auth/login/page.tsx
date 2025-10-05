14d834584b6e79ee9a404510953d79bf060cfb5e
Trong Dat <datnt700@gmail.com>
2025-10-05 10:39:25 +0200

    fix: review code

--- apps/webapp/src/app/auth/login/page.tsx
+++ apps/webapp/src/app/auth/login/page.tsx
@@ -2,8 +2,16 @@ import { GalleryVerticalEnd } from 'lucide-react';
 
 import { LoginForm } from '../_components';
 import React from 'react';
+import { auth } from '@/auth';
+import { getLocale } from 'next-intl/server';
+import { redirect } from 'next/navigation';
 
 export default async function LoginPage() {
+  const [session, locale] = await Promise.all([auth(), getLocale()]);
+
+  if (session) {
+    redirect(`/${locale}`);
+  }
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
